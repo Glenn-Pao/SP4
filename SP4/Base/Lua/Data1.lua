76a73d30a13de689ea7c6a8f704b11d25eb0470c
Pyrolf <pyroflame11@gmail.com>
2016-03-01 18:09:34 +0800

    Solve Conflicts

--- SP4/Base/Lua/Data1.lua
+++ SP4/Base/Lua/Data1.lua
@@ -4,16 +4,16 @@ Use these values to initialise the Data's Infomations
 
 ifNew = 0
 
-heroPositionX = 61
-heroPositionY = 331.676
-heroAnimationDir = 1
-noOfJellybeans = 867
+heroPositionX = 402
+heroPositionY = 509.674
+heroAnimationDir = 0
+noOfJellybeans = 959
 heroMapOffsetX = 0
 heroMapOffsetY = 50
 
 DifficultyUnlocked0 = 4
 DifficultyUnlocked1 = 4
-DifficultyUnlocked2 = 3
+DifficultyUnlocked2 = 4
 DifficultyUnlocked3 = 3
 
 guardianCleared = 1
